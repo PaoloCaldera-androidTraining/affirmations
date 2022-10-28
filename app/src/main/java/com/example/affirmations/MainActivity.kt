79d8ec394a8302dadd77fca57e7dd6d65d3cf6e8
Paolo Caldera <paolo.caldera@keypartner.com>
2022-10-28 17:54:50 +0200

    Let the RecyclerView use the ItemAdapter

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
 import com.example.affirmations.databinding.ActivityMainBinding
 
@@ -15,5 +16,11 @@ class MainActivity : AppCompatActivity() {
         // Inflate the layout through the viewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Set up the recycler view and its adapter
+        val recyclerView = binding.recyclerView
+        val recyclerViewAdapter = ItemAdapter(this, Datasource().loadAffirmations())
+        recyclerView.adapter = recyclerViewAdapter
+        recyclerView.setHasFixedSize(true)
     }
 }
